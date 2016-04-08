ce43b457137c9f2ff50370170179126ace53cbe0
JohnAustin-msft <johnau@microsoft.com>
2016-04-08 13:44:05 -0700

    New settings dialog

--- SfbWellBaby/app/src/main/java/com/microsoft/office/sfb/sfbwellbaby/SettingsDialog.java
+++ SfbWellBaby/app/src/main/java/com/microsoft/office/sfb/sfbwellbaby/SettingsDialog.java
@@ -1,29 +1,33 @@
 package com.microsoft.office.sfb.sfbwellbaby;
 
-
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SettingsDialog.OnFragmentInteractionListener} interface
+ * to handle interaction events.
  * Use the {@link SettingsDialog#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class SettingsDialog extends DialogFragment {
-    // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private String mMeetingURI;
+    private EditText mEditURI;
+    private OnFragmentInteractionListener mListener;
 
     public SettingsDialog() {
         // Required empty public constructor
@@ -36,7 +40,6 @@ public class SettingsDialog extends DialogFragment {
      * @param param1 Parameter 1.
      * @return A new instance of fragment SettingsDialog.
      */
-    // TODO: Rename and change types and number of parameters
     public static SettingsDialog newInstance(String param1) {
         SettingsDialog fragment = new SettingsDialog();
         Bundle args = new Bundle();
@@ -49,16 +52,77 @@ public class SettingsDialog extends DialogFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
+            mMeetingURI = getArguments().getString(ARG_PARAM1);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(mParam1);
-        return textView;
+        // Inflate the layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_settings_dialog, container, false);
+
+        ButterKnife.inject(getActivity());
+        EditText meetingURIEditor = (EditText) rootView.findViewById(R.id.meetingURIText);
+        SharedPreferences settings = getActivity().getSharedPreferences(getActivity().getString(R.string.meetingURIKey), 0);
+        String meetingURI = settings.getString(getActivity().getString(R.string.meetingURIKey), "");
+        if (meetingURI.isEmpty())
+            meetingURIEditor.setText(mMeetingURI);
+        else
+            meetingURIEditor.setText(meetingURI);
+
+        // Watch for button clicks.
+        Button button = (Button) rootView.findViewById(R.id.closeButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // We need an Editor object to make preference changes.
+                // All objects are from android.context.Context
+                String preferenceKey = getActivity().getString(R.string.meetingURIKey);
+                SharedPreferences settings = getActivity().getSharedPreferences(preferenceKey, 0);
+                SharedPreferences.Editor editor = settings.edit();
+                String newString = ((EditText) rootView.findViewById(R.id.meetingURIText)).getText().toString();
+                editor.putString(preferenceKey, newString);
+
+                // Commit the edits!
+                editor.commit();
+                if (mListener != null) {
+                    mListener.onFragmentInteraction(newString);
+                }
+            }
+        });
+
+        return rootView;
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(String newMeetingURI);
+    }
 }
--- SfbWellBaby/app/src/main/java/com/microsoft/office/sfb/sfbwellbaby/wellbabyreport.java
+++ SfbWellBaby/app/src/main/java/com/microsoft/office/sfb/sfbwellbaby/wellbabyreport.java
@@ -2,7 +2,6 @@ package com.microsoft.office.sfb.sfbwellbaby;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -16,10 +15,11 @@ import butterknife.OnClick;
 import static com.microsoft.office.sfb.sfbwellbaby.R.id.toolbar;
 
 
-public class wellbabyreport extends AppCompatActivity {
+public class wellbabyreport extends AppCompatActivity implements SettingsDialog.OnFragmentInteractionListener {
     Intent videoIntent = null;
     @InjectView(toolbar)
     protected Toolbar mToolBar;
+    android.support.v4.app.DialogFragment mSettingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +40,7 @@ public class wellbabyreport extends AppCompatActivity {
         videoIntent = new Intent(this, SkypeCall.class);
         startActivity(videoIntent);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -48,6 +49,7 @@ public class wellbabyreport extends AppCompatActivity {
 
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         try {
@@ -60,8 +62,9 @@ public class wellbabyreport extends AppCompatActivity {
 
                     ft.addToBackStack(null);
 
-                    DialogFragment settingsFragment = SettingsDialog.newInstance("https://meet.lync.com/microsoft/johnau/H7TW81MG");
-                    settingsFragment.show(getSupportFragmentManager(), "settings");
+                    mSettingsFragment = SettingsDialog.newInstance("https://meet.lync.com/<yourDomain>/<yourAlias>/<YourInstance>");
+                    mSettingsFragment.show(getSupportFragmentManager(), "settings");
+
                     return true;
                 default:
                     return super.onOptionsItemSelected(item);
@@ -75,5 +78,12 @@ public class wellbabyreport extends AppCompatActivity {
         }
         return true;
 
+    }
+
+    @Override
+    public void onFragmentInteraction(String newMeetingURI) {
+        if (mSettingsFragment.isVisible()) {
+            mSettingsFragment.dismiss();
+        }
     }
 }
