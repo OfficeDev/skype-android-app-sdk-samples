520630860c913788c37115e4f78f0c75b9b11fb6
JohnAustin-msft <johnau@microsoft.com>
2016-04-13 19:23:28 -0700

    Moved JoinConversation to the SkypeCall activity

--- SfbWellBaby/app/src/main/java/com/microsoft/office/sfb/sfbwellbaby/SkypeCall.java
+++ SfbWellBaby/app/src/main/java/com/microsoft/office/sfb/sfbwellbaby/SkypeCall.java
@@ -1,9 +1,11 @@
 package com.microsoft.office.sfb.sfbwellbaby;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.TextureView;
 import android.view.View;
 
 import com.microsoft.office.sfb.appsdk.Conversation;
@@ -11,20 +13,30 @@ import com.microsoft.office.sfb.appsdk.SFBException;
 import com.microsoft.office.sfb.sfbwellbaby.SkypeAPI.SkypeManager;
 import com.microsoft.office.sfb.sfbwellbaby.SkypeAPI.SkypeManagerImpl;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import butterknife.ButterKnife;
 
-public class SkypeCall extends AppCompatActivity implements SkypeManager.SkypeConversationJoinCallback,
-
-SkypeManager.SkypeVideoReady{
+public class SkypeCall extends AppCompatActivity
+        implements SkypeManager.SkypeConversationJoinCallback, SkypeManager.SkypeVideoReady,
+        SkypeCallFragment.OnFragmentInteractionListener{
 
     SkypeManagerImpl mSkypeManagerImpl;
     Conversation mAnonymousMeeting;
     SkypeCallFragment mCallFragment = null;
 
+
     ConversationPropertyChangeListener conversationPropertyChangeListener = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Get the singleton instance of the skype manager
+        mSkypeManagerImpl = SkypeManagerImpl.getInstance(
+                getApplicationContext(),
+                this,this);
+
         setContentView(R.layout.activity_skype_call);
         ButterKnife.inject(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -33,10 +45,7 @@ SkypeManager.SkypeVideoReady{
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //Get the singleton instance of the skype manager
-        mSkypeManagerImpl = SkypeManagerImpl.getInstance(
-                getApplicationContext(),
-                this,this);
+
 
         mCallFragment = SkypeCallFragment.newInstance(mSkypeManagerImpl);
         FragmentTransaction fragmentTransaction = getSupportFragmentManager()
@@ -68,14 +77,63 @@ SkypeManager.SkypeVideoReady{
     }
 
 
+    /**
+     * Connect to an existing Skype for Business meeting with the URI from
+     * shared preferences. Normally the URI is supplied to the mobile device
+     * via some mechanism outside the scope of this sample.
+     * @param callView The fragment that hosts the incoming and outgoing video
+     */
+    private void startConversation(View callView){
+        SharedPreferences settings = getSharedPreferences(getString(R.string.meetingURIKey), 0);
+        String meetingURIString = settings.getString(getString(R.string.meetingURIKey), "");
+        URI meetingURI = null;
+        try {
+            meetingURI = new URI(meetingURIString);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        try {
+            mSkypeManagerImpl.joinConversation(
+                    meetingURI,
+                    getString(R.string.fatherName),
+
+                    (TextureView) callView
+                            .findViewById(
+                                    R.id.selfParticipantVideoView));
+        } catch (SFBException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void onSkypeIncomingVideoReady() {
 
     }
 
+    /**
+     * The fragment TextureView that displays the preview is ready.
+     * Start the outgoing video.
+     */
     @Override
     public void onSkypeOutgoingVideoReady() {
         mSkypeManagerImpl.startOutgoingVideo();
+    }
+
+    @Override
+    public void onFragmentInteraction(View callView, String newMeetingURI) {
+        startConversation(callView);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        //If there is an active meeting and the user can leave then
+        //leave the meeting before closing this activity
+        if (mAnonymousMeeting != null && mAnonymousMeeting.canLeave())
+            try {
+                mAnonymousMeeting.leave();
+            } catch (SFBException e) {
+                e.printStackTrace();
+            }
     }
 }
--- SfbWellBaby/app/src/main/java/com/microsoft/office/sfb/sfbwellbaby/SkypeCallFragment.java
+++ SfbWellBaby/app/src/main/java/com/microsoft/office/sfb/sfbwellbaby/SkypeCallFragment.java
@@ -1,20 +1,15 @@
 package com.microsoft.office.sfb.sfbwellbaby;
 
 import android.annotation.SuppressLint;
-import android.content.SharedPreferences;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.microsoft.office.sfb.appsdk.SFBException;
 import com.microsoft.office.sfb.sfbwellbaby.SkypeAPI.SkypeManagerImpl;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 import butterknife.InjectView;
 import butterknife.OnClick;
@@ -28,20 +23,24 @@ import static com.microsoft.office.sfb.sfbwellbaby.R.id.pauseVideoButton;
 public class SkypeCallFragment extends Fragment {
 
     @InjectView(pauseVideoButton)
-    Button mPauseButton;
+    protected Button mPauseButton;
 
     private SkypeManagerImpl mSkypeManagerImpl = null;
+    private OnFragmentInteractionListener mListener;
+    View mRootView;
 
-    public SkypeCallFragment(){}
+
+    public SkypeCallFragment() {
+    }
 
     @SuppressLint("ValidFragment")
     public SkypeCallFragment(SkypeManagerImpl skypeManager) {
         mSkypeManagerImpl = skypeManager;
     }
+
     /**
      * Create the Video fragment.
      *
-
      * @return A new instance of fragment VideoFragment.
      */
     public static SkypeCallFragment newInstance(SkypeManagerImpl skypeManager) {
@@ -49,35 +48,45 @@ public class SkypeCallFragment extends Fragment {
 
         return fragment;
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView =  inflater.inflate(R.layout.fragment_skype_call, container, false);
+        mRootView = inflater.inflate(R.layout.fragment_skype_call, container, false);
+        mListener.onFragmentInteraction(mRootView, "inflated");
+        return mRootView;
+    }
 
-        SharedPreferences settings = getActivity().getSharedPreferences(getString(R.string.meetingURIKey), 0);
-        String meetingURIString = settings.getString(getString(R.string.meetingURIKey), "");
-        URI meetingURI = null;
-        try {
-            meetingURI = new URI(meetingURIString);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
         }
-
-        try {
-            mSkypeManagerImpl.joinConversation(
-                    meetingURI,
-                    getActivity().getString(R.string.fatherName),
-
-                    (TextureView) rootView.findViewById(R.id.selfParticipantVideoView));
-        } catch (SFBException e) {
-            e.printStackTrace();
-        }
-        return rootView;
     }
 
     @OnClick(pauseVideoButton)
-    public void onClickVideoPause(View button){
+    public void onClickVideoPause(Button button) {
 
         mSkypeManagerImpl.stopOutgoingVideo();
     }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(View rootView, String newMeetingURI);
+    }
+
 }
