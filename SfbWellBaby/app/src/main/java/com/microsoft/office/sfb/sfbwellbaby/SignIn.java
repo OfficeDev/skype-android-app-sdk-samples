10abfc35011ed91da533ac1e61f96311b5072790
JohnAustin-msft <johnau@microsoft.com>
2016-04-27 13:31:12 -0700

    layout changes to wellbaby activity title bar area

--- SfbWellBaby/app/src/main/java/com/microsoft/office/sfb/sfbwellbaby/SignIn.java
+++ SfbWellBaby/app/src/main/java/com/microsoft/office/sfb/sfbwellbaby/SignIn.java
@@ -15,7 +15,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
@@ -30,16 +29,6 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static android.Manifest.permission.CALL_PHONE;
-import static android.Manifest.permission.CAMERA;
-import static android.Manifest.permission.MODIFY_AUDIO_SETTINGS;
-import static android.Manifest.permission.READ_CALENDAR;
-import static android.Manifest.permission.READ_CONTACTS;
-import static android.Manifest.permission.READ_PHONE_STATE;
-import static android.Manifest.permission.RECORD_AUDIO;
-import static android.Manifest.permission.WRITE_CONTACTS;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 /**
  * A login screen that offers login via email/password.
@@ -111,48 +100,8 @@ public class SignIn extends AppCompatActivity implements LoaderCallbacks<Cursor>
     }
 
     private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
             return true;
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            requestPermissions(new String[]{
-                                    READ_CONTACTS,
-                                    READ_PHONE_STATE,
-                                    RECORD_AUDIO,
-                                    CALL_PHONE,
-                                    READ_CALENDAR,
-                                    WRITE_CONTACTS,
-                                    CAMERA,
-                                    MODIFY_AUDIO_SETTINGS,
-                                    WRITE_EXTERNAL_STORAGE
-                                    }, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS,
-                    READ_PHONE_STATE,
-                    RECORD_AUDIO,
-                    CALL_PHONE,
-                    READ_CALENDAR,
-                    WRITE_CONTACTS,
-                    CAMERA,
-                    MODIFY_AUDIO_SETTINGS,
-                    WRITE_EXTERNAL_STORAGE}, REQUEST_READ_CONTACTS);
-
-            //requestPermissions(
-            // this,
-            // new String[]{
-            // android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            // Manifest.permission.ACCESS_COARSE_LOCATION},
-            // REQUEST_CODE_WRITE_EXTERNAL_STORAGE);
         }
         return false;
     }
@@ -199,12 +148,7 @@ public class SignIn extends AppCompatActivity implements LoaderCallbacks<Cursor>
             cancel = true;
         }
 
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        }
+
 
         if (cancel) {
             // There was an error; don't attempt login and focus the first
@@ -223,13 +167,11 @@ public class SignIn extends AppCompatActivity implements LoaderCallbacks<Cursor>
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
+        return true;
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return true;
     }
 
     /**
